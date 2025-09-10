441f56b4713eec9bf45de2bb07f16ed084314b34
Bismark <nhanhaasare94@gmail.com>
2025-09-10 10:24:44 +0000

    Delete alx-react-app-new/src/components directory

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
-      <p>&copy; {new Date().getFullYear()} My Favorite Cities App</p>
-    </footer>
-  );
-}
-
-export default Footer;

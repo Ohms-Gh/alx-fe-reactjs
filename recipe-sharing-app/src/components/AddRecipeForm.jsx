4500f33821df9bcf91b43d59c8785ce9b40c4839
Bismark <nhanhaasare94@gmail.com>
2025-09-11 07:29:50 +0000

    AddRecipeForm.jsx

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from 'alx-fe-reactjs/recipe-sharing-app/store/recipeStore';
 
 const AddRecipeForm = () => {
   const addRecipe = useRecipeStore((state) => state.addRecipe);

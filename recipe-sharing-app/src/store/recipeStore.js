4956d50d52e44c847cede5092fc53a46a724ba4f
Bismark <nhanhaasare94@gmail.com>
2025-09-11 07:37:39 +0000

    Update recipeStore.js

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,8 +1,19 @@
-import { create } from 'zustand';
+import create from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === updatedRecipe.id ? { ...r, ...updatedRecipe } : r
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
+
   setRecipes: (recipes) => set({ recipes }),
 }));

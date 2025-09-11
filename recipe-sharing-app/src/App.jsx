2fb06e3dd98583ce46b0be39663ec9ad390a418c
Bismark <nhanhaasare94@gmail.com>
2025-09-11 07:52:13 +0000

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,16 @@
+// src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
+import SearchBar from './components/SearchBar';
 
 function Home() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>🍲 Recipe Sharing App</h1>
+      <SearchBar />
       <AddRecipeForm />
       <RecipeList />
     </div>

36b38dd23c699c784a34671c9f52b3b23223344e
Bismark <nhanhaasare94@gmail.com>
2025-09-11 07:38:27 +0000

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,29 @@
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from '../store/recipeStore';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
-  if (recipes.length === 0) {
+  if (!recipes.length) {
     return <p>No recipes yet. Add one!</p>;
   }
 
   return (
     <div>
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="recipe-card">
-          <h3>{recipe.title}</h3>
+        <div key={recipe.id} className="recipe-card" style={{ marginBottom: '1rem' }}>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
+
+          <div>
+            <Link to={`/recipes/${recipe.id}/edit`} style={{ marginRight: 8 }}>
+              Edit
+            </Link>
+            <DeleteRecipeButton recipeId={recipe.id} />
+          </div>
         </div>
       ))}
     </div>

10c82cad1044d289cdac920e54fa9726250033ad
Bismark <nhanhaasare94@gmail.com>
2025-09-17 08:06:29 +0000

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users?q";
+const BASE_URL = "https://api.github.com";
 
 export async function searchUsers({ username, location, minRepos, page = 1 }) {
   let query = "";
@@ -9,17 +9,38 @@ export async function searchUsers({ username, location, minRepos, page = 1 }) {
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>${minRepos} `;
 
+  const headers = import.meta.env.VITE_APP_GITHUB_API_KEY
+    ? { Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}` }
+    : {};
+
   try {
     const response = await axios.get(`${BASE_URL}/search/users`, {
       params: {
         q: query.trim(),
         page,
-        per_page: 30, 
+        per_page: 30,
       },
+      headers,
     });
     return response.data;
   } catch (error) {
     console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
+export async function fetchUserData(username) {
+  const headers = import.meta.env.VITE_APP_GITHUB_API_KEY
+    ? { Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}` }
+    : {};
+
+  try {
+    const response = await axios.get(`${BASE_URL}/users/${username}`, {
+      headers,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user:", error);
     throw error;
   }
 }

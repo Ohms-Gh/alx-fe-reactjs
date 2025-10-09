80cdd6d262d9cd60825b90a15d30f14ec6e14b66
Bismark <nhanhaasare94@gmail.com>
2025-10-09 08:34:56 +0000

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,9 @@
-import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }

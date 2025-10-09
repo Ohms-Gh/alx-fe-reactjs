abc515e7be7d22eb64a61236d0b9359f86d97751
Bismark <nhanhaasare94@gmail.com>
2025-10-09 09:18:15 +0000

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PostsComponent from "./PostsComponent";
 
 const queryClient = new QueryClient();
@@ -11,6 +12,8 @@ function App() {
         <h1 className="text-3xl font-bold mb-4">React Query Demo</h1>
         <PostsComponent />
       </div>
+
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }

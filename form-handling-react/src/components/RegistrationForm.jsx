dae7ccf48f6d7cfb587c00d752e80083b03d4acb
Bismark <nhanhaasare94@gmail.com>
2025-10-09 09:06:39 +0000

    Update RegistrationForm.jsx

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react";
 
 const RegistrationForm = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { username, email, password } = formData;
 
     if (!username || !email || !password) {
       setError("All fields are required!");
@@ -32,7 +20,7 @@ const RegistrationForm = () => {
       const response = await fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({ username, email, password }),
       });
       const data = await response.json();
       console.log("User registered:", data);
@@ -50,8 +38,8 @@ const RegistrationForm = () => {
         type="text"
         name="username"
         placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
+        value={username}           
+        onChange={(e) => setUsername(e.target.value)}
         className="border p-2 rounded"
       />
 
@@ -59,8 +47,8 @@ const RegistrationForm = () => {
         type="email"
         name="email"
         placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
+        value={email}              
+        onChange={(e) => setEmail(e.target.value)}
         className="border p-2 rounded"
       />
 
@@ -68,8 +56,8 @@ const RegistrationForm = () => {
         type="password"
         name="password"
         placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
+        value={password}      
+        onChange={(e) => setPassword(e.target.value)}
         className="border p-2 rounded"
       />
 

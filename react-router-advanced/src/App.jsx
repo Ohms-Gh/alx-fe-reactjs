6785617a7791c8608924494a05ed36eab08a2e84
Bismark <nhanhaasare94@gmail.com>
2025-10-09 09:40:02 +0000

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile/Profile.jsx';
+import ProfileDetails from './pages/Profile/ProfileDetails.jsx';
+import ProfileSettings from './pages/Profile/ProfileSettings.jsx';
+import Blog from './pages/Blog/Blog.jsx';
+import BlogPost from './pages/Blog/BlogPost.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Protected Profile Route */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<Profile />}>
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
+        </Route>
+
+        {/* Dynamic Blog Route */}
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+
+        {/* Redirect unknown routes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+}

6228c491c3af0be99a5de9a0db65243c7e6ea0ca
Bismark <nhanhaasare94@gmail.com>
2025-10-09 08:32:01 +0000

    Update RegistrationForm.jsx

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -29,7 +29,6 @@ const RegistrationForm = () => {
     setError("");
 
     try {
-      // Mock API request (replace with your actual API endpoint)
       const response = await fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
